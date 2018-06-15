fcc55fbf942f37363719c2468440ecfb36a25f5f
Guy Kremnitzer <guykrem@gmail.com>
2018-06-16 00:03:13 +0300

    remove icons package

--- src/components/player-standing.js
+++ src/components/player-standing.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import cn from 'classnames'
-import { Icon } from 'react-icons-kit'
-import { iosFootball } from 'react-icons-kit/ionicons/'
-import { questionCircle } from 'react-icons-kit/fa/'
+import { Icon } from 'antd'
 function PlayerStanding({
   player,
   success = false,
@@ -14,11 +12,9 @@ function PlayerStanding({
     <div className={cn('player-standing', { success })}>
       {score !== null && <span className="score">{score.toFixed(1)}</span>}
       <span className={cn('name')}>{player.name}</span>
-      {isGoalsSuccess && (
-        <Icon icon={iosFootball} style={{ verticalAlign: 'middle', marginRight: 5 }} />
-      )}
+      {isGoalsSuccess && <span style={{ verticalAlign: 'middle', marginRight: 5 }}>⚽</span>}
       {isBonusSuccess && (
-        <Icon icon={questionCircle} style={{ verticalAlign: 'middle', marginRight: 5 }} />
+        <Icon type="question-circle" style={{ verticalAlign: 'middle', marginRight: 5 }} />
       )}
     </div>
   )
